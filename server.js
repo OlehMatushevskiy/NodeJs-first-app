05916cdbdaffb47d80e97aab0cb8bdd50b3c0b31
OlehMatushevskiy <matushevskiy072008@gmail.com>
2026-03-12 16:17:14 +0200

    add heap used log

--- server.js
+++ server.js
@@ -5,6 +5,10 @@ const server = http.createServer((req,res) => {
 
     console.log(req.method);
     console.log(req.headers['user-agent']);
+
+    const memoryData = process.memoryUsage();
+    
+    console.log(`Used heap: ${(memoryData.heapUsed / 1024 / 1024).toFixed(2)} MB`);
     res.end('Server is running on 3000 port');
 });
 
